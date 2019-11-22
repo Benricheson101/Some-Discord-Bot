4868f9a93f8c6f3a791fbb8631c7c86d79cae7bf
Ben Richeson <36977340+Benricheson101@users.noreply.github.com>
2019-11-22 16:05:23 -0500

    Actual deploy command (broke something last time somehow)

--- src/cmds/deploy.js
+++ src/cmds/deploy.js
@@ -1,3 +1,7 @@
+const { promisify } = require('util');
+const { exec } = require('child_process');
+const asyncExec = promisify(exec);
+
 module.exports.config = {
 	name: "deploy",
 	aliases: [],
@@ -5,7 +9,28 @@ module.exports.config = {
 	guildOnly: false
 };
 
-module.exports.run = async (client, message, args) => {
 
+
+module.exports.run = async (client, message, args) => {
+	let m = await message.channel.send("Deploy command received...")
+	console.log("Deploy command received...")
+		.then(() => {
+			m.edit("Updating code...");
+			console.log("Updating code from Git");
+			return asyncExec("git fetch origin && git reset --hard origin/production");
+		})
+		.then((x) => {
+			console.log("Updating NPM modules");
+			m.edit("Updating NPM modules...");
+			return asyncExec("npm i --production");
+		})
+		.then((x) => {
+			return asyncExec("git rev-parse HEAD");
+		})
+		.then((x22) => {
+			console.log("Shutting down...");
+			m.edit("Shutting down...");
+			return asyncExec("process.exit(0)")
+		})
 };
 
