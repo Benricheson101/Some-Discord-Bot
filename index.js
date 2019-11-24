ca87fa1a1b3b8be72cc67639a01067077e18889a
Ben Richeson <36977340+Benricheson101@users.noreply.github.com>
2019-11-23 22:51:54 -0500

    no sleep

--- index.js
+++ index.js
@@ -49,3 +49,18 @@ fs.readdir("./src/events/", (err, files) => {
 
 client.login(process.env.TOKEN)
 	.catch(console.error);
+
+// ----- Glitch Stuff -----
+
+// -- Don't Sleep Please --
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+	console.log(Date.now() + " Ping Received");
+	response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+	http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
