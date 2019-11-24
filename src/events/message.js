b618f8c2ba92d576862d6ef9c8d611c7c836c20a
Ben Richeson <36977340+Benricheson101@users.noreply.github.com>
2019-11-23 20:13:31 -0500

    A few more various small things

--- src/events/message.js
+++ src/events/message.js
@@ -29,16 +29,21 @@ module.exports = async (client, message) => {
 		message.channel.send(CONSTANTS.errors.generic);
 	}
 
+	/**
+	 * Handles any errors that occur while running a command.
+	 * @param {Error} err - The error
+	 */
 	function createError (err) {
+		let embed = new (require("discord.js")).RichEmbed()
+			.setAuthor("something happened")
+			.setTitle(err.message)
+			.setDescription(`\`\`\`js\n${(err.stack).length >= 2048 ? (err.stack).substring(0, 2000) + " content too long..." : err.stack}\`\`\``)
+			.setColor("DARK_RED")
+			.setTimestamp()
+			.setFooter(message.content);
+
 		console.error(err);
-		// client.channels.find((c) => c.id === CONSTANTS.config.logChannel).send(message.content,
-		message.channel.send(message.content,
-			new (require("discord.js")).RichEmbed()
-				.setAuthor("something happened")
-				.setTitle(err.message)
-				.setDescription(`\`\`\`js\n${(err.stack).length >= 2048 ? (err.stack).substring(0, 2000) + " content too long..." : err.stack}\`\`\``)
-				.setColor("DARK_RED")
-				.setTimestamp()
-		);
+		client.channels.find((c) => c.id === CONSTANTS.config.logChannel).send(embed);
+		message.channel.send(embed);
 	}
 };
