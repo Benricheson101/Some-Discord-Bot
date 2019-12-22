7222d463ba61400a9246ff2a96c3d6d32edbbac5
Ben Richeson <benricheson101@gmail.com>
2019-12-21 19:30:24 -0500

    bean th bot

--- src/cmds/bean.js
+++ src/cmds/bean.js
@@ -14,7 +14,7 @@ module.exports.run = async (client, message, args) => {
 	if (!args[0] || !getMember(args[0])) return message.channel.send(":x: You must mention someone or provide an ID of the user you would like to bean!");
 	let user = getMember(args[0]);
 
-	if (message.member.roles.highest.comparePositionTo(user.roles.highest) < 0) return message.channel.send(":x: I cannot bean this user.");
+	if (message.member.roles.highest.comparePositionTo(user.roles.highest) < 0 && user.id !== client.user.id) return message.channel.send(":x: I cannot bean this user.");
 
 	await message.channel.send(`:white_check_mark: ${user} was beaned by ${message.author} with reason: ${(args.slice(1)).join(" ") || "no reason"}`);
 
